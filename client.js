2b174da5c41431f054327084d85174010ec9a49c
Nick Johnstone <ncwjohnstone@gmail.com>
2016-09-08 16:19:56 +1200

    Functional tmux in web

--- client.js
+++ client.js
@@ -1,10 +1,50 @@
 import ws from 'ws';
+import {ansi_to_html, escape_for_html} from 'ansi_up';
+import Terminal from 'terminal.js';
 
 const socket = new WebSocket('ws://localhost:3000');
+const terminal = new Terminal({ columns: 100, rows: 30 });
 
 socket.onmessage = function (event) {
-  console.log('received: ', event.data);
+  event.data.toString('utf-8').split('\n').forEach(line => {
+    if (line.startsWith("%output")) {
+      line = line.replace(/%output %\d+ /, '');
+      line = line.replace(/\\(\d+)/g, (_, match) => String.fromCharCode(parseInt(match, 8)))
+      terminal.write(line);
 
-  socket.send('wow!');
+      document.querySelector('.output').innerHTML = terminal.toString('html');
+    }
+  });
 };
 
+document.addEventListener('keydown', (event) => {
+  event.preventDefault();
+
+  let keyToSend = event.key;
+
+  if (keyToSend === 'Backspace') {
+    keyToSend = 'BSpace';
+  }
+
+  if (keyToSend === ' ') {
+    keyToSend = 'Space';
+  }
+
+  if (keyToSend === '"') {
+    keyToSend = '\"'
+  }
+
+  if (keyToSend === 'Control' || keyToSend === 'Shift') {
+    return;
+  }
+
+  if (keyToSend.startsWith('Arrow')) {
+    keyToSend = keyToSend.replace('Arrow', '');
+  }
+
+  if (event.ctrlKey) {
+    keyToSend = `C-${keyToSend}`;
+  }
+
+  socket.send(keyToSend);
+});
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import ws from 'ws';
 import express from 'express';
 import cors from 'cors';
 import http from 'http';
+import childProcess from 'child_process';
 
 const app = express();
 
@@ -17,11 +18,28 @@ server.listen(3000);
 
 const socketServer = new ws.Server({server});
 
+
 socketServer.on('connection', (socket) => {
+  const tmux = childProcess.spawn('tmux', ['-C'], {tmux: true});
+
   socket.on('message', (message) => {
-    console.log('received:', message);
+    console.log('received:', JSON.stringify(message));
+    tmux.stdin.write('send-keys ' + JSON.stringify(message) + '\n');
   });
 
-  socket.send('hello world');
+  socket.on('close', () => {
+    tmux.kill();
+  });
+
+  tmux.stdout.on('data', (data) => {
+    data = data.toString('utf8');
+    console.log('tmux:', data);
+    socket.send(data.toString('utf8'));
+  });
+
+  tmux.on('close', () => {
+    console.log('tmux exited');
+  });
 });
+
 
